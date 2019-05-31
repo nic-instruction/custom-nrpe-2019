e16db5bea7ca7d0c25c0f1f0dfb1c86e0fb6946e
nic-instruction <nicole.bade@seattlecolleges.edu>
2019-05-30 18:52:27 -0700

    Create number_of_processes.sh

---
+++ number_of_processes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This is a script meant to let us test NRPE
+# plugins we write
+
+status=$(ps aux | grep -c "")                                  # Change the status to test different alert states
+
+if [ $status -le "100" ]; then
+    echo "STATUS:OK"
+    exit 0;
+    
+  elif [ $status -ge "150" ]; then
+    echo "STATUS:CRITICAL"
+    exit 2;
+    
+  elif [ $status -ge "101" ] ; then
+    echo "STATUS:WARNING"
+    exit 1;
+    
+else
+   echo "STATUS:UNKNOWN"
+   exit 3;
+fi
