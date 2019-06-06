94a4afce907b105aa6d3f841347068842aa5c15f
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-05 20:08:44 -0700

    Create connectiontest.sh

---
+++ connectiontest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+
+if DBNAME=postgres PGPORT=5432 PGUSER=myprojectuser PGPASS=password
+then
+        echo "[OK]"
+        exit 0;
+else
+        echo "[FAIL]"
+        exit1;
+        
+fi
