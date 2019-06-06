70d79ee9f06671d61356b274515069ade2344ae7
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-05 20:17:23 -0700

    test from django to postgres

--- connectiontest.sh
+++ connectiontest.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
+# test from django to postgres
 
-if DBNAME=postgres PGPORT=5432 PGUSER=myprojectuser PGPASS=password
-then
+connection=$(DBNAME=postgres PGPORT=5432 PGUSER=myprojectuser PGPASS=password)
+
+if [ $connection == $connection ] ; then
         echo "[OK]"
         exit 0;
-else
+        
+elif [ $connection != $connection ]; then
         echo "[FAIL]"
-        exit1;
+        exit 2;
+
+else
+        echo "STATUS:UNKNOWN"
+        exit 3;
         
 fi
